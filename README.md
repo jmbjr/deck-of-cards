b0fb4159660896f1a4cb569dc06f6712881e99a8
John Boyle <boylejm@gmail.com>
2016-10-10 22:57:08 -0700

    initial commit from raidrace repo

---
+++ README.md
@@ -0,0 +1,12 @@
+Raid Race is a temporary project name
+
+Card game similar to the game Mille Bornes except in this game you would be racing to defeat a dungeon boss
+
+Instead of miles, you could be dealing damage. First team to deal enough damage to kill the boss wins the round.
+
+Hazards would be what you would expect from a typical MMO raid
+
+Safeties could be epic tank, epic healer, epic AOE DPS, epic DPS... Something like that
+
+Remedies would be the counter part to their hazards
+
---
+++ update.sh
@@ -0,0 +1,102 @@
+#!/bin/sh
+
+echo "Delete existing pngs?"
+read run_delete
+
+echo "Run NanDeck?"
+read run_nandeck
+
+echo "scp data to remote?"
+read run_scp
+
+remote="kvm"
+nandeck="/c/dev/nandeck_v1.22/nanDECK.exe"
+local="/c/Users/John/Documents/My Games/Tabletop Simulator"
+ttsdir="./tts/"
+savedir="./save/"
+htmlfile="${ttsdir}/index.html"
+
+#set up index.html file
+echo "<HTML>" > ${htmlfile}
+echo "<BODY text=#000000 link=#FF0000 bgColor=#9BB4B8><P align=center>" >> ${htmlfile}
+echo "<font size=+2>RaidRace Decks</font>" >> ${htmlfile}
+
+echo "<br>" >> ${htmlfile}
+
+echo "--------------------" | tee -a ${htmlfile} && echo "<br>" >> ${htmlfile}
+echo "Delete existing pngs? = ${run_delete}" | tee -a ${htmlfile} && echo "<br>" >> ${htmlfile}
+echo "Run NanDeck? = ${run_nandeck}" | tee -a ${htmlfile} && echo "<br>" >> ${htmlfile}
+echo "scp data to remote? = ${run_scp}" | tee -a ${htmlfile} && echo "<br>" >> ${htmlfile}
+
+echo "NANDECK BUILD SCRIPT" | tee -a ${htmlfile} && echo "<br>" >> ${htmlfile}
+date  | tee -a ${htmlfile} && echo "<br>" >> ${htmlfile}
+echo "--------------------" | tee -a ${htmlfile} && echo "<br>" >> ${htmlfile}
+
+#delete existing pngs
+if ls ${ttsdir}/*.png 1> /dev/null 2>&1; then
+  if [ "${run_delete}" == "y" ];then
+    echo "REMOVING EXISTING *.PNG FILES FROM ${ttsdir} DIRECTORY!" | tee -a ${htmlfile} && echo "<br>" >> ${htmlfile}
+    ls -l ${ttsdir}/*.png
+    rm ${ttsdir}/*.png
+    echo "PNG FILES DELETED!" | tee -a ${htmlfile} && echo "<br>" >> ${htmlfile}
+  else
+    echo "SKIPPING DELETING PNG FILES" | tee -a ${htmlfile} && echo "<br>" >> ${htmlfile}
+  fi
+else
+  echo "NO PNG FILES IN ${ttsdir}" | tee -a ${htmlfile} && echo "<br>" >> ${htmlfile}
+fi
+
+#build decks.
+if [ "${run_nandeck}" == "y" ];then
+  echo "BUIDLING DECKS!" | tee -a ${htmlfile} && echo "<br>" >> ${htmlfile}
+  ls -l *.nde
+  for deck in *.nde
+  do
+    deckname="${deck%.*}"
+    echo "BUILDING $deckname" | tee -a ${htmlfile} && echo "<br>" >> ${htmlfile}
+    $nandeck $deck //exec
+    if ls ${ttsdir}/$deckname.png 1> /dev/null 2>&1; then
+      echo "$deckname SUCCESSFULLY BUILT!" | tee -a ${htmlfile} && echo "<br>" >> ${htmlfile}
+      echo "<br><img src=\"$deckname.png\" alt=\"$deckname\" height="70%">" >> ${htmlfile} && echo "<br>" >> ${htmlfile}
+    else
+      echo "$deckname FAILED TO BUILD!" | tee -a ${htmlfile} && echo "<br>" >> ${htmlfile}
+    fi
+    echo "<br>" >> ${htmlfile}
+  done
+else
+  echo "SKIPPING RUNNING NANDECK" | tee -a ${htmlfile} && echo "<br>" >> ${htmlfile}
+fi
+
+if [ "${run_scp}" == "y" ]; then
+  #push to remote
+  if ls ${ttsdir}/*.png 1> /dev/null 2>&1; then
+    echo "COPYING FILES TO REMOTE SERVER!" | tee -a ${htmlfile} && echo "<br>" >> ${htmlfile}
+    ls -l ${ttsdir}/*.png
+    echo "REMOTE= $remote" | tee -a ${htmlfile} && echo "<br>" >> ${htmlfile}
+    
+    ssh "${remote}"  'rm raidrace/img/*.png && rm raidrace/saves/*.json'
+    scp ${ttsdir}/*.png  "${remote}":raidrace/img/
+    scp ${htmlfile}  "${remote}":raidrace/img/
+    scp ${savedir}/*.json "${remote}":raidrace/saves/
+  
+    #clear local cache
+    if ls "${local}"/Mods/Images/*raidrace*.png 1> /dev/null 2>&1; then
+      echo "DELETING LOCAL CACHE" | tee -a ${htmlfile} && echo "<br>" >> ${htmlfile}
+      ls -l "${local}"/Mods/Images/*raidrace*.png
+      rm "${local}"/Mods/Images/*raidrace*.png
+    else
+      echo "NO LOCAL FILES TO DELETE!" | tee -a ${htmlfile} && echo "<br>" >> ${htmlfile}
+    fi
+    
+    #install current save file to Save folder
+    cp ${savedir}/*.json "${local}"/Saves/Chest
+  else
+    echo "NO PNG IMAGES FOUND IN CURRENT DIRECTORY!" | tee -a ${htmlfile} && echo "<br>" >> ${htmlfile}
+    ls -l
+  fi
+else
+  echo "SKIPPING SCPing FILES TO REMOTE" | tee -a ${htmlfile} && echo "<br>" >> ${htmlfile}
+fi
+
+echo "</BODY>" >> ${htmlfile}
+echo "</HTML>" >> ${htmlfile}
